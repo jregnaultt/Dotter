eba6fcb58823c79be417028eb971f0778c5314a2
Javier Regnault <regnaultjavier2@gmail.com>
2026-07-27 23:46:42 -0400

    Configurar soporte de variables de entorno (.env), archivo .env.example y .gitignore

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,12 @@
 import os
 from pydantic import BaseModel
 
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
+
 class Settings(BaseModel):
     PROJECT_NAME: str = "Fighting Game Sprite Generator API"
     VERSION: str = "1.0.0"
